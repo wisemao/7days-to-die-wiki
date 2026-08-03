e231a1a140b12c7c672e8a18cd8c18dd8e1d03a6
wisemao <wisemao@users.noreply.github.com>
2026-08-03 17:09:17 +0800

    chore: 移除临时扫描脚本

--- scan-med.mjs
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'fs';
-const t = readFileSync('F:/SteamLibrary/steamapps/common/7 Days to Die/Data/Config/items.xml', 'utf-8');
-const i = t.indexOf('<item name="medicalBandage"');
-const end = t.indexOf('</item>', i);
-console.log('=== medicalBandage 完整 ===');
-console.log(t.slice(i, end + 8).replace(/\t/g, '  '));
